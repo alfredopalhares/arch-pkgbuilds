6d239c3e9644d52ae55c98b34c7d3d818b20c841
Alfredo Palhares <alfredo@palhares.me>
2024-04-06 18:33:47 +0100

    upgpkg: joplin 2.14.20-1
    upstream release

--- joplin/joplin.sh
+++ joplin/joplin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-readonly joplin_dir="/usr/share/joplin/node_modules/joplin/app/"
+readonly joplin_dir="/usr/lib/node_modules/joplin/app/"
 
 if [[ ! -d $joplin_dir ]]; then
-  echo "Cannot find ${joplin_dir}"
-  exit 1
+    echo "Cannot find ${joplin_dir}"
+    exit 1
 fi
 
 cd $joplin_dir
